0011de073d21853035c3ad6b0d281a14f9f1f0cd
JT <email2james@gmail.com>
2026-06-02 21:46:07 +1000

    Delete custom_components/Aruba_IAP_Device_Tracker directory

--- custom_components/Aruba_IAP_Device_Tracker/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for Aruba_AP_Device_Tracker."""
-
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-DOMAIN = "Aruba_AP_Device_Tracker"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
